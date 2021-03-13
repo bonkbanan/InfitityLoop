230907e669839bb8f605545b21e089478be875b2
dench <denchikbalan@gmail.com>
2021-03-13 13:28:21 +0200

    inf loop + max integer

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,8 +7,8 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         Random random = new Random();
         while (true){
-            System.out.println(random.nextInt(10000));
-            Thread.sleep(10);
+            System.out.println(random.nextInt(Integer.MAX_VALUE));
+            Thread.sleep(1);
         }
     }
 }
